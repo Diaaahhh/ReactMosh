6612a14d53d1438715eeb8ec38ec94ce66bdcfbe
Diaaahhh <farihaanjumdia2000@gmail.com>
2026-07-01 18:06:51 +0600

    ind=0 is active

--- frontend/src/Component/ListGroup.tsx
+++ frontend/src/Component/ListGroup.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import type { MouseEvent } from "react";
+// event handler
+ const handleClick= (event: MouseEvent )=>{
+  console.log(event)
+}
 
 function ListGroup() {
-  let names = ["Dia", "Muna", "Mono"];
+  const names = ["Dia", "Muna", "Mono"];
   // names = [];
 const messageFunc= ( message)=>{
   return names.length == 0 && <p>{message}</p> || <h4>This is your squad</h4>
@@ -11,8 +16,8 @@ const messageFunc= ( message)=>{
       
       {messageFunc("no squad found")}
       <ul className="list-group">
-        {names.map((name) => (
-          <li className="list-group-item">{name}</li>
+        {names.map((name, index) => (
+          <li className= {index==0?"list-group-item active":"list-group-item"} onClick={handleClick} key={index}> {name}</li>
         ))}
       </ul>
     </>
