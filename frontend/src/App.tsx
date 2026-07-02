7d88bba9899108d5c63f27c219940fe8e36517f6
Diaaahhh <farihaanjumdia2000@gmail.com>
2026-07-02 17:58:40 +0600

    Start interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
     <div>
       <Message/>
       <ListGroup/>
+      <ListGroup/>
     </div>
   )
 }
--- frontend/src/Component/ListGroup.tsx
+++ frontend/src/Component/ListGroup.tsx
@@ -1,12 +1,16 @@
-import React from "react";
-import type { MouseEvent } from "react";
+import React, { useState } from "react";
+// import type { MouseEvent } from "react";
 // event handler
- const handleClick= (event: MouseEvent )=>{
-  console.log(event)
-}
+//  const handleClick= (event: MouseEvent )=>{
+//   console.log(event)
+// }
+// interface ListGroupProps{
+// heading: 
+// }
 
 function ListGroup() {
   const names = ["Dia", "Muna", "Mono"];
+  const [selectedIndex, setSelectedIndex]= useState(-1)
   // names = [];
 const messageFunc= ( message)=>{
   return names.length == 0 && <p>{message}</p> || <h4>This is your squad</h4>
@@ -17,7 +21,7 @@ const messageFunc= ( message)=>{
       {messageFunc("no squad found")}
       <ul className="list-group">
         {names.map((name, index) => (
-          <li className= {index==0?"list-group-item active":"list-group-item"} onClick={handleClick} key={index}> {name}</li>
+          <li className= {index==selectedIndex?"list-group-item active":"list-group-item"} onClick={()=>{setSelectedIndex(index)}} key={index}> {name}</li>
         ))}
       </ul>
     </>
